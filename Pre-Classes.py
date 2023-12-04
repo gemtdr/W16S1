67e0a5e667354cdb758894a594e6523a5a3e4bab
gemtdr <123492499+gemtdr@users.noreply.github.com>
2023-12-03 18:41:15 -0600

    Add files via upload

---
+++ Pre-Classes.py
@@ -0,0 +1,25 @@
+
+
+class Person:
+
+    def __init__(self):
+        
+        self.name = ""
+        self.age = ""
+        self.school = ""
+
+
+#Build a class called Student that inherits from Person with "major" attribute.
+#Make overloaded print statement for Student.
+
+#Build a class called Faculty that inherits from Person with "title" attribute.
+#Make overloaded print statement for Faculty.
+
+
+        
+
+
+
+if __name__ == "__main__":
+
+    
